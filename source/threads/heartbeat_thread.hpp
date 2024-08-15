16f9299d1605ff2d04b4a12143bf3c4d3f9d5ee5
Petr Malanik <Malanik.Petr.gm@gmail.com>
2024-08-15 11:00:44 +0200

    heartbeat_thread: add doxygen comments

--- source/threads/heartbeat_thread.hpp
+++ source/threads/heartbeat_thread.hpp
@@ -16,18 +16,32 @@
 namespace fra = cpp_freertos;
 
 /**
- * @brief Green LED hearthbeat thread
+ * @brief Yellow LED hearth-beat thread signaling that device is alive
  */
 class LED_heartbeat_thread : public fra::Thread {
 public:
-
+    /**
+     * @brief Construct a new led heartbeat thread and starts is
+     *
+     * @param gpio_led_number   Number of GPIO pin where LED is connected
+     * @param delay             Delay between LED state change
+     */
     explicit LED_heartbeat_thread(uint gpio_led_number, uint32_t delay);
 
 private:
+    /**
+     * @brief Pointer to GPIO object representing LED
+     */
     GPIO * const led;
 
+    /**
+     * @brief Delay between LED state change
+     */
     const uint32_t delay;
 
 protected:
+    /**
+     * @brief   Main function of thread, executed after thread starts
+     */
     virtual void Run();
 };
