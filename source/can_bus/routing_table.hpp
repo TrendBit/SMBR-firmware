2fa66df23b87c8dde7c0126327cb984bc8020f99
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-01-03 10:12:29 +0100

    routing_table: update routing table with heater codes

--- source/can_bus/routing_table.hpp
+++ source/can_bus/routing_table.hpp
@@ -25,14 +25,21 @@ inline static const std::unordered_map<Codes::Command_admin, Codes::Component> A
  *          STL version of unordered_map is used because ETL is not placed into flash memory
  */
 inline static const std::unordered_map<Codes::Message_type, Codes::Component> Routing_table = {
-    { Codes::Message_type::Device_reset,                Codes::Component::Common_core      },
-    { Codes::Message_type::Device_usb_bootloader,       Codes::Component::Common_core      },
-    { Codes::Message_type::Device_can_bootloader,       Codes::Component::Common_core      },
-    { Codes::Message_type::Probe_modules_request,       Codes::Component::Common_core      },
-    { Codes::Message_type::Ping_request,                Codes::Component::Common_core      },
-    { Codes::Message_type::Core_temperature_request,    Codes::Component::Common_core      },
-    { Codes::Message_type::Core_load_request,           Codes::Component::Common_core      },
-    { Codes::Message_type::LED_set_intensity,           Codes::Component::LED_panel        },
-    { Codes::Message_type::LED_get_intensity_request,   Codes::Component::LED_panel        },
-    { Codes::Message_type::LED_get_temperature_request, Codes::Component::LED_panel        },
+    { Codes::Message_type::Device_reset,                          Codes::Component::Common_core      },
+    { Codes::Message_type::Device_usb_bootloader,                 Codes::Component::Common_core      },
+    { Codes::Message_type::Device_can_bootloader,                 Codes::Component::Common_core      },
+    { Codes::Message_type::Probe_modules_request,                 Codes::Component::Common_core      },
+    { Codes::Message_type::Ping_request,                          Codes::Component::Common_core      },
+    { Codes::Message_type::Core_temperature_request,              Codes::Component::Common_core      },
+    { Codes::Message_type::Core_load_request,                     Codes::Component::Common_core      },
+
+    { Codes::Message_type::LED_set_intensity,                     Codes::Component::LED_panel        },
+    { Codes::Message_type::LED_get_intensity_request,             Codes::Component::LED_panel        },
+    { Codes::Message_type::LED_get_temperature_request,           Codes::Component::LED_panel        },
+
+    { Codes::Message_type::Heater_set_intensity,                  Codes::Component::Bottle_heater    },
+    { Codes::Message_type::Heater_get_intensity_request,          Codes::Component::Bottle_heater    },
+    { Codes::Message_type::Heater_set_target_temperature,         Codes::Component::Bottle_heater    },
+    { Codes::Message_type::Heater_get_target_temperature_request, Codes::Component::Bottle_heater    },
+    { Codes::Message_type::Heater_get_plate_temperature_request,  Codes::Component::Bottle_heater    },
 };
