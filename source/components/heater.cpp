bac24473c49511a0a4a6c972978e71d87d1ada9b
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-01-03 10:12:05 +0100

    heater: add implementation of heater control without regulation

--- source/components/heater.cpp
+++ source/components/heater.cpp
@@ -3,15 +3,36 @@
 Heater::Heater(uint gpio_in1, uint gpio_in2, float pwm_frequency):
     Component(Codes::Component::Bottle_heater),
     Message_receiver(Codes::Component::Bottle_heater),
-    control_bridge(new DC_HBridge(new PWM(gpio_in1, pwm_frequency, 0.0, true), new PWM(gpio_in2, pwm_frequency, 0.0, true)))
+    control_bridge(new DC_HBridge_PIO(gpio_in1, gpio_in2, PIO_machine(pio0,3), pwm_frequency)),
+    heater_sensor(new Thermistor(new ADC_channel(ADC_channel::RP2040_ADC_channel::CH_3, 3.30f), 3950, 100000, 25, 30000)),
+    heater_fan(new GPIO(11, GPIO::Direction::Out))
 {
     control_bridge->Coast();
+    heater_fan->Set(false);
 }
 
 float Heater::Intensity(float requested_intensity){
-    this->intensity = requested_intensity; //std::clamp(requested_intensity, -1.0f, 1.0f);
+    // Reduce intensity of higher then maximal allowed
+    this->intensity = std::clamp(requested_intensity, -intensity_limit, intensity_limit);
+
+    // Start heater fan if heater is in use
+    if(intensity != 0){
+        heater_fan->Set(true);
+    } else {
+        heater_fan->Set(false);
+    }
+
+    // Set intensity of heater
     control_bridge->Speed(intensity);
     return intensity;
+}
+
+float Heater::Intensity() const {
+    return intensity;
+}
+
+float Heater::Temperature(){
+     return heater_sensor->Temperature();
 }
 
 bool Heater::Receive(CAN::Message message){
@@ -20,5 +41,40 @@ bool Heater::Receive(CAN::Message message){
 }
 
 bool Heater::Receive(Application_message message){
-    return false;
+    switch (message.Message_type()){
+        case Codes::Message_type::Heater_set_intensity:{
+            App_messages::Heater::Set_intensity set_intensity;
+
+            if (!set_intensity.Interpret_data(message.data)){
+                Logger::Print("Heater_set_intensity interpretation failed");
+                return false;
+            }
+            Logger::Print(emio::format("Heater intensity set to: {:03.1f}", set_intensity.intensity));
+            Intensity(set_intensity.intensity);
+            return true;
+        }
+
+        case Codes::Message_type::Heater_get_intensity_request:{
+            App_messages::Heater::Get_intensity_response intensity_response(Intensity());
+            Send_CAN_message(intensity_response);
+            return true;
+        }
+
+        case Codes::Message_type::Heater_set_target_temperature:
+            return false;
+
+        case Codes::Message_type::Heater_get_target_temperature_request:
+            return false;
+
+        case Codes::Message_type::Heater_get_plate_temperature_request:{
+            float temp = Temperature();
+            Logger::Print(emio::format("Heater plate temperature: {:05.2f}˚C", temp));
+            App_messages::Heater::Get_plate_temperature_response plate_temperature(temp);
+            Send_CAN_message(plate_temperature);
+            return true;
+        }
+
+        default:
+            return false;
+    }
 }
--- source/components/heater.hpp
+++ source/components/heater.hpp
@@ -7,23 +7,79 @@
 
 #pragma once
 
-#include "codes/codes.hpp"
-
 #include "can_bus/message_receiver.hpp"
-
 #include "components/component.hpp"
-#include "components/motors/dc_hbridge.hpp"
+#include "components/motors/dc_hbridge_pio.hpp"
+#include "components/thermometers/thermistor.hpp"
+#include "hal/adc/adc_channel.hpp"
+#include "hal/pio.hpp"
+#include "logger.hpp"
+
+#include "codes/codes.hpp"
+#include "codes/messages/heater/set_intensity.hpp"
+#include "codes/messages/heater/get_intensity_response.hpp"
+#include "codes/messages/heater/set_target_temperature.hpp"
+#include "codes/messages/heater/get_target_temperature_response.hpp"
+#include "codes/messages/heater/get_plate_temperature_response.hpp"
 
 class Heater: public Component, public Message_receiver {
 private:
-    DC_HBridge * control_bridge;
+    /**
+     * @brief H-bridge for control of heater
+     */
+    DC_HBridge_PIO * control_bridge;
 
+    /**
+     * @brief Maximal intensity of heater limited by heat dissipation of driver
+     */
+    const float intensity_limit = 0.8f;
+
+    /**
+     * @brief Current intensity of heater
+     */
     float intensity = 0.0;
 
-public:
-    Heater(uint gpio_in1, uint gpio_in2, float pwm_frequency = 50.0f);
+    /**
+     * @brief   Thermistor for temperature measurement of heatspreader of heater
+     */
+    Thermistor * const heater_sensor;
 
+    /**
+     * @brief GPIO for control of heater fan (fan regulation temperature of heater heatsink)
+     */
+    GPIO * const heater_fan;
+
+public:
+    /**
+     * @brief Construct a new Heater object
+     *
+     * @param gpio_in1      GPIO number of input 1 of H-bridge, Forward
+     * @param gpio_in2      GPIO number of input 2 of H-bridge, Reverse
+     * @param pwm_frequency Frequency of PWM signal for control of heater, around 100K Hz seems optimal
+     */
+    Heater(uint gpio_in1, uint gpio_in2, float pwm_frequency);
+
+    /**
+     * @brief   Set intensity of heater, positive value means heating, negative cooling
+     *
+     * @param requested_intensity   Intensity of heater, range <-1, 1>
+     * @return float            Real intensity set to heater, limited by maximal intensity (power dissipation of driver)
+     */
     float Intensity(float requested_intensity);
+
+    /**
+     * @brief   Get current intensity of heater
+     *
+     * @return float    Current intensity of heater
+     */
+    float Intensity() const;
+
+    /**
+     * @brief Read temperature from thermistor connected to heatspreader of heater
+     *
+     * @return float    Temperature in Celsius
+     */
+    float Temperature();
 
     /**
      * @brief   Receive message implementation from Message_receiver interface for General/Admin messages (normal frame)
