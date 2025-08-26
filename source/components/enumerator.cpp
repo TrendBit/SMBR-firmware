4c8640eb2530e8fa58891e794eecc0ac422e6e74
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-08-27 01:02:58 +0200

    enumerator: new components handling module instance enumeration

---
+++ source/components/enumerator.cpp
@@ -0,0 +1,67 @@
+#include "enumerator.hpp"
+
+Enumerator::Enumerator(Codes::Instance instance_type) :
+    Component(Codes::Component::Enumerator),
+    current_instance(instance_type)
+{
+    if (instance_type == Codes::Instance::Exclusive) {
+        Logger::Notice("Enumerator initialized as Exclusive instance");
+    } else if (instance_type == Codes::Instance::Undefined) {
+        Logger::Notice("Enumerator initialized as Undefined instance, will attempt to enumerate");
+    } else {
+        Logger::Notice("Enumerator initialized as Instance {}", magic_enum::enum_name(instance_type));
+    }
+    // @todo Load instance from memory if available, need support in memory component first
+}
+
+Enumerator::Enumerator(Codes::Instance instance_type, uint button_pin, uint rgb_led_pin) :
+    Enumerator(instance_type)
+{
+    // @todo Implement button and RGB LED support
+    enumeration_led = new Addressable_LED(rgb_led_pin, PIO_machine(pio0, 0), 1);
+    enumeration_button = new GPIO_IRQ(button_pin, GPIO::Direction::In);
+    enumeration_button->Enable_IRQ(GPIO_IRQ::IRQ_level::Rising_edge, [this](){
+        Enumeration_button_pressed();
+    });
+    Logger::Warning("Enumerator with button and RGB LED not implemented yet");
+    UNUSED(button_pin);
+
+    Set_RGB_LED_color(0xff, 0xff, 0xff);
+}
+
+Codes::Instance Enumerator::Instance() const{
+    return current_instance;
+}
+
+bool Enumerator::Stable() const{
+    if (current_instance == Codes::Instance::Exclusive){
+        return true;
+    }
+
+    return false;
+}
+
+bool Enumerator::Valid() const{
+    if (current_instance == Codes::Instance::Exclusive){
+        return true;
+    }
+
+    if (current_instance == Codes::Instance::Undefined ||
+        current_instance == Codes::Instance::All ||
+        current_instance == Codes::Instance::Reserved) {
+        return false;
+    }
+
+    return true;
+}
+
+void Enumerator::Set_RGB_LED_color(uint8_t red, uint8_t green, uint8_t blue) const {
+    if (enumeration_led != nullptr) {
+        enumeration_led->Set_all(red, green, blue);
+    }
+}
+
+void Enumerator::Enumeration_button_pressed(){
+    Logger::Trace("Enumeration button pressed");
+    Set_RGB_LED_color(0xff, 0x00, 0x00);
+}
---
+++ source/components/enumerator.hpp
@@ -0,0 +1,137 @@
+/**
+ * @file enumerator.hpp
+ * @author Petr Malaník (TheColonelYoung(at)gmail(dot)com)
+ * @version 0.1
+ * @date 26.08.2025
+ */
+
+#pragma once
+
+#define UNUSED(x) (void)(x)
+
+#include "codes/codes.hpp"
+#include "components/component.hpp"
+#include "components/led/addressable_LED.hpp"
+#include "hal/gpio/gpio_irq.hpp"
+#include "logger.hpp"
+
+/**
+ * @brief Component used for enumeration of modules and their instances in system
+ *        Instance should be unique in whole system for each module.
+ *        Exclusive instances are fixed and cannot be changed (only one in system should exists).
+ *        Enumerated instances (instance_x) can be determined by this component from undefined instance.
+ *        Instances can be mapped to colors by which modules instance can be determined by user.
+ *              There are special colors:
+ *                  White - Undefined instance
+ *                  Red   - Enumeration issue
+ */
+class Enumerator: public Component {
+private:
+    /**
+     * @brief   Instance enumeration of module, initialized by derived class calling constructor
+     *          Can be changed by enumeration process if initalized as undefined
+     */
+    Codes::Instance current_instance;
+
+    /**
+     * @brief   Delay (ms) after enumeration process determines there are no conflicts and instance can be confirmed
+     */
+    uint32_t enumeration_delay_ms = 2000;
+
+    /**
+     * @brief   Pointer to RGB LED used for instance indication and enumeration process
+     */
+    Addressable_LED * enumeration_led = nullptr;
+
+    /**
+     * @brief   Pointer to button used to trigger enumeration change
+     */
+    GPIO_IRQ * enumeration_button = nullptr;
+
+public:
+    /**
+     * @brief   Construct a new Enumerator component with defined enumeration
+     *              which is not intended to be changed by user (no button or indication)
+     *
+     * @param instance_type Instance enumeration of module
+     */
+    Enumerator(Codes::Instance instance_type = Codes::Instance::Undefined);
+
+    /**
+     * @brief   Construct a new Enumerator component with button and RGB LED indication
+     *              Button is used to trigger enumeration process (increment instance)
+     *              RGB LED is used to indicate current instance by color
+     *
+     * @param instance_type     Instance enumeration of module
+     * @param button_pin        GPIO pin number of button used to trigger enumeration process
+     * @param rgb_led_pin       GPIO pin number of RGB LED used to indicate current instance by color
+     */
+    Enumerator(Codes::Instance instance_type, uint button_pin, uint rgb_led_pin);
+
+    /**
+     * @brief  Get current instance enumeration of module
+     *
+     * @return Codes::Instance  Current instance enumeration
+     */
+    Codes::Instance Instance() const;
+
+    /**
+     * @brief   Check if current instance is stable,not in process of enumeration or undefined/error state
+     *          Exclusive instance is considered always stable
+     *
+     * @return true     Initial enumeration was finalized
+     * @return false    Stable instance is not defined, in error state or in process of enumeration
+     */
+    bool Stable() const;
+
+    /**
+     * @brief   Check if current instance is correct, not undefined, in error state or in process of enumeration
+     *          Exclusive instance is considered correct
+     *
+     * @return true     Current instance is Valid
+     * @return false    Current instance is undefined, in error state or still in process of enumeration
+     */
+    bool Valid() const;
+
+private:
+    /**
+     * @brief   Load last used instance from persistent EEPROM memory
+     *
+     * @return Codes::Instance
+     */
+    Codes::Instance Load_instance_from_memory();
+
+    /**
+     * @brief   Save current instance to persistent EEPROM memory
+     *
+     * @return true     Instance was saved successfully
+     * @return false    Instance saving failed
+     */
+    bool Save_instance_to_memory() const;
+
+    /**
+     * @brief   Preview current instance as color on enumeration RGB LED
+     */
+    void Show_instance_color() const;
+
+    /**
+     * @brief   Set color of enumeration RGB LED
+     *
+     * @param red   Red component of color (0-255)
+     * @param green Green component of color (0-255)
+     * @param blue  Blue component of color (0-255)
+     */
+    void Set_RGB_LED_color(uint8_t red, uint8_t green, uint8_t blue) const;
+
+    /**
+     * @brief   Process which will check usage of initial address and confirm it / select new / set error state
+     * @todo    Enumeration process needs to be specified, this is only a placeholder
+     */
+    void Enumerate();
+
+    /**
+     * @brief   Method called when enumeration button is pressed by user
+     *          Should lead to increment of instance and new enumeration validation
+     */
+    void Enumeration_button_pressed();
+};
