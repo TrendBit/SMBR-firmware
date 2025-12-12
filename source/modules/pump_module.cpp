304bad52254a8569ac7b65df17c653691bb21572
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-12-12 18:59:24 +0100

    pump_module: integrate pump controller and its configuration

--- source/modules/pump_module.cpp
+++ source/modules/pump_module.cpp
@@ -1,7 +1,15 @@
 #include "pump_module.hpp"
 
 Pump_module::Pump_module():
-    Base_module(Codes::Module::Pump_module, new Enumerator(Codes::Module::Pump_module,memory,Codes::Instance::Undefined, 22, 23), 24, 18, 19),
+    Base_module(
+        Codes::Module::Pump_module,
+        new Enumerator(
+            Codes::Module::Pump_module,
+            memory,
+            Codes::Instance::Undefined,
+            enumeration_button_gpio,
+            enumeration_rgb_led_gpio),
+        activity_green_led_gpio, i2c_sda_gpio, i2c_scl_gpio),
     board_thermistor(new Thermistor(new ADC_channel(ADC_channel::RP2040_ADC_channel::CH_1, 3.30f), 3950, 10000, 25, 5100))
 {
     Setup_components();
@@ -9,6 +17,31 @@ Pump_module::Pump_module():
 
 void Pump_module::Setup_components(){
     Logger::Debug("Pump module component setup");
+
+    uint8_t pump_count;
+    // Detect number of pumps based on configuration pin
+    GPIO config_pin = GPIO(config_detection_pin);
+    config_pin.Set_direction(GPIO::Direction::In);
+    config_pin.Set_pulls(true, false);
+    if (config_pin.Read()) {
+        pump_count = 4;
+    } else {
+        pump_count = 2;
+    }
+
+    Logger::Notice("Module configuration: {} pumps", pump_count);
+
+    auto pump_1 = new Pump(15, 14, 16, 1000.0f, 0.1f);
+    auto pump_2 = new Pump(11, 10, 17, 1000.0f, 0.1f);
+    auto pump_3 = new Pump( 7,  6, 20, 1000.0f, 0.1f);
+    auto pump_4 = new Pump( 2,  3, 21, 1000.0f, 0.1f);
+
+    pump_controller = new Pump_controller(
+        pump_count == 2 ?
+            etl::vector<Pump *,8>{ pump_1, pump_2 } :
+            etl::vector<Pump *,8>{ pump_1, pump_2, pump_3, pump_4 }
+    );
+
     Logger::Warning("No components to setup");
 }
 
--- source/modules/pump_module.hpp
+++ source/modules/pump_module.hpp
@@ -11,12 +11,55 @@
 #include "codes/codes.hpp"
 #include "logger.hpp"
 
-class Pump_module: public Base_module {
+#include "components/pumps.hpp"
 
+/**
+ * @brief   Module responsible for control of additional pumps
+ *          Module can be configure for 2 or 4 pumps based on configuration pin state
+ *          This module can have multiple instances in system
+ */
+class Pump_module: public Base_module {
+private:
     /**
      * @brief   Thermistor to measure onboard temperature
      */
     Thermistor * const board_thermistor;
+
+    /**
+     * @brief   GPIO of MCU which is connected to enumeration button
+     */
+    static constexpr uint enumeration_button_gpio = 22;
+
+    /**
+     * @brief   GPIO of MCU which is connected to enumeration RGB LED
+     */
+    static constexpr uint enumeration_rgb_led_gpio = 23;
+
+    /**
+     * @brief   GPIO of MCU which is connected to activity green LED
+     */
+    static constexpr uint activity_green_led_gpio = 24;
+
+    /**
+     * @brief   GPIO of MCU which is connected to I2C SDA line
+     */
+    static constexpr uint i2c_sda_gpio = 18;
+
+    /**
+     * @brief   GPIO of MCU which is connected to I2C SCL line
+     */
+    static constexpr uint i2c_scl_gpio = 19;
+
+    /**
+     * @brief   GPIO of MCU which is connected to pump configuration pin
+     *          Based on value of this pin number of pumps is set up.
+     */
+    static const uint config_detection_pin = 25;
+
+    /**
+     * @brief   Component responsible for control of pumps connected to module
+     */
+    Pump_controller * pump_controller;
 
 public:
     /**
