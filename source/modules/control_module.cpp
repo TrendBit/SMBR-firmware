17e47ba11ad74f6476521f4827fe959396ccdff0
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-06-02 11:19:26 +0200

    pump_module: connect to CLI

--- source/modules/control_module.cpp
+++ source/modules/control_module.cpp
@@ -128,5 +128,125 @@ std::optional<float> Control_module::Board_temperature(){
     return temp;
 }
 
+
+bool Parse_channel(const std::string& arg, CLI_service& cli, uint8_t& selected_channel){
+    if(arg.size() != 1){
+        cli.Print_ln(dye::red("invalid channel!"));
+        return false;
+    }
+    
+    auto arg_char = arg[0];
+
+    if( arg_char < '0' || arg_char > '9'){
+        cli.Print_ln(dye::red("invalid channel!"));
+        return false;
+    }
+    
+    selected_channel = arg_char - '0';
+    return true;
+}
+
+bool Parse_channels(
+    const std::vector<std::string>& args,
+    CLI_service& cli, 
+    bool fill_on_empty, 
+    std::vector<uint8_t>& selected_channels,
+    size_t skip_args = 0
+){    
+    
+    const size_t channel_count = 10;
+    if(args.size()==skip_args){
+        if(fill_on_empty){
+            selected_channels.reserve(channel_count);
+            
+            for(uint8_t i =0; i<channel_count; i++){
+                selected_channels.push_back(i);
+            }
+        }else{
+            cli.Print_ln(dye::red("missing channels"));
+            return false;
+        }
+    }else{
+        for (size_t i = skip_args; i < args.size(); i++){
+            const auto& arg = args[i];
+            uint8_t channel;
+            
+            if (not Parse_channel(arg, cli, channel)){
+                return false;
+            }
+            
+            if (channel > (channel_count - 1)){
+                return false;
+            }
+            
+            selected_channels.push_back(channel);
+        }
+    }
+    return true;
+}
+
 void Control_module::Setup_cli(CLI_service& cli) const {
+    if( led_panel ){
+        cli.Bind("led_set_intensity",[this, &cli](std::vector<std::string> args){
+            if(not CLI_service::Check_argument_count(args,cli,2)){
+                return;
+            }
+            
+            float intensity = 0.0;
+            if(not CLI_service::Parse_argument(args[0],cli,intensity)){
+                return;
+            }
+            
+            
+            std::vector<uint8_t> channels;
+            if(not Parse_channels(args,cli,false,channels,1)){
+                return;
+            }
+            
+            for(const auto& channel : channels){
+                if(led_panel->Set_intensity(channel, intensity)){
+                    cli.Print_ln("success");
+                }else{
+                    cli.Print_error("Set_intensity failed");
+                }
+            }
+        },"set intensity of selected channels","intensity(float) channel channel...");
+        
+        cli.Bind("led_get_intensity",[this, &cli](std::vector<std::string> args){
+            if(not CLI_service::Check_argument_count(args,cli,0)){
+                return;
+            }
+            
+            std::vector<uint8_t> channels;
+            if(not Parse_channels(args,cli,false,channels)){
+                return;
+            }
+            
+            for(const auto& channel : channels){
+                std::optional<float> intensity = led_panel->Get_intensity(channel);
+                
+                if(intensity.has_value()){
+                    cli.Print_ln(emio::format("channel {}: {}",channel, intensity));
+                }else{
+                    cli.Print_error("Get_intensity failed");
+                }
+            }
+        },"set intensity of selected channels","channel channel...");
+        
+        cli.Bind("led_power_limited",[this, &cli](){
+            if(led_panel->Power_limited()){
+                cli.Print_ln("not limited");
+            }else{
+                cli.Print_ln(dye::yellow("limited"));
+            }
+        },"Detect if power of LED illumination is limited by power budget");
+        
+        cli.Bind("led_temp_limited",[this, &cli](){
+            if(led_panel->Temperature_limited()){
+                cli.Print_ln("not limited");
+            }else{
+                cli.Print_ln(dye::yellow("limited"));
+            }
+        },"Detect if power of LED illumination is limited by temperature of module");
+    }
 }
--- source/modules/pump_module.cpp
+++ source/modules/pump_module.cpp
@@ -1,4 +1,9 @@
 #include "pump_module.hpp"
+#include "cli.hpp"
+#include "tools/color.hpp"
+#include <cstdint>
+#include <optional>
+#include <string>
 
 Pump_module::Pump_module():
     Base_module(
@@ -69,5 +74,253 @@ std::optional<float> Pump_module::Board_temperature(){
     adc_mutex->Unlock();
     return temp;
 }
+
+/**
+ * @brief parses 
+ */
+bool Parse_pump_index(const std::string& arg, uint8_t pump_count, CLI_service& cli, uint8_t& selected_pump_index){
+    if(arg.size() != 1){
+        cli.Print_error("invalid pump index!");
+        return false;
+    }
+    
+    auto arg_char = arg[0];
+
+    if( arg_char <= '0' || arg_char > ('0' + pump_count)){
+        cli.Print_error("pump index out of range!");
+        return false;
+    }
+    
+    selected_pump_index = arg_char - '0';
+    return true;
+}
+
+bool Parse_pump_indexes(
+    const std::vector<std::string>& args,
+    uint8_t pump_count, 
+    CLI_service& cli, 
+    bool fill_on_empty, 
+    std::vector<uint8_t>& selected_pump_indexes,
+    size_t skip_args = 0
+){
+    if(args.size()==skip_args){
+        if(fill_on_empty){
+            selected_pump_indexes.reserve(pump_count);
+            
+            for(uint8_t i =0; i<pump_count; i++){
+                selected_pump_indexes.push_back(i+1);
+            }
+        }else{
+            cli.Print_error("missing pump indexes");
+            return false;
+        }
+    }else{
+        for (size_t i = skip_args; i < args.size(); i++){
+            const auto& arg = args[i];
+            uint8_t pump_index;
+            
+            if (not Parse_pump_index(arg, pump_count, cli, pump_index)){
+                return false;
+            }
+            
+            selected_pump_indexes.push_back(pump_index);
+        }
+    }
+    return true;
+}
+
+void Print_pump_value(CLI_service& cli, uint8_t pump_index, std::optional<float> value){
+    if(value.has_value()){
+        cli.Print_ln(emio::format("Pump {}: {}",pump_index, value.value()));
+    }else{
+        cli.Print_error("failed to retrieve value");
+    }
+}
+
 void Pump_module::Setup_cli(CLI_service& cli) const {
+    cli.Bind("pump_stop",[this,&cli](std::vector<std::string> args)->void{
+        uint8_t pump_count = pump_controller->Pump_count();
+        
+        if(not CLI_service::Check_argument_count(args, cli, 1, pump_count)){
+            return;
+        }
+        
+        for (const auto& arg : args){
+            uint8_t pump_index = 0;
+            if(not Parse_pump_index(arg, pump_count, cli, pump_index)){
+                continue;
+            }
+            
+            if(not pump_controller->Stop(pump_index)){
+                cli.Print_error("Stop failed");
+                continue;
+            }
+            
+            cli.Print_ln("success");
+        }
+    },"stops all or selected pumps","[pump_index pump_index ...]?");
+    
+    cli.Bind("pump_count",[this,&cli]()->void{
+        uint8_t pump_count = pump_controller->Pump_count();
+        
+        cli.Print_ln(std::to_string(pump_count));
+    },"get the number of installed pumps");
+    
+    cli.Bind("pump_get_speed",[this,&cli](std::vector<std::string> args)->void{
+        uint8_t pump_count = pump_controller->Pump_count();
+        
+        if(not CLI_service::Check_argument_count(args, cli, 0, pump_count)){
+            return;
+        }
+        
+        std::vector<uint8_t> selected_indexes;
+        
+        if(not Parse_pump_indexes(args, pump_count, cli, true, selected_indexes)){
+            return;
+        }
+        
+        for(const auto& pump_index : selected_indexes){
+            std::optional<float> speed = pump_controller->Get_speed(pump_index);
+            
+            Print_pump_value(cli,pump_index,speed);
+        }
+    },"get pump speed for all, or selected pumps","[pump_index pump_index ...]?");
+    
+    cli.Bind("pump_get_flowrate",[this,&cli](std::vector<std::string> args)->void{
+        uint8_t pump_count = pump_controller->Pump_count();
+        
+        if(not CLI_service::Check_argument_count(args, cli, 0, pump_count)){
+            return;
+        }
+        
+        std::vector<uint8_t> selected_indexes;
+        
+        if(not Parse_pump_indexes(args, pump_count, cli, true, selected_indexes)){
+            return;
+        }
+        
+        for(const auto& pump_index : selected_indexes){
+            std::optional<float> flowrate = pump_controller->Get_flowrate(pump_index);
+            
+            Print_pump_value(cli,pump_index,flowrate);
+        }
+    },"get flowrate for all, or selected pumps","[pump_index pump_index ...]?");
+    
+    cli.Bind("pump_get_max_flowrate",[this,&cli](std::vector<std::string> args)->void{
+        uint8_t pump_count = pump_controller->Pump_count();
+        
+        if(not CLI_service::Check_argument_count(args, cli, 0, pump_count)){
+            return;
+        }
+        
+        std::vector<uint8_t> selected_indexes;
+        
+        if(not Parse_pump_indexes(args, pump_count, cli, true, selected_indexes)){
+            return;
+        }
+        
+        for(const auto& pump_index : selected_indexes){
+            std::optional<float> flowrate = pump_controller->Max_flowrate(pump_index);
+            
+            Print_pump_value(cli,pump_index,flowrate);
+        }
+    },"get maximal flowrate for all, or selected pumps","[pump_index pump_index ...]?");
+    
+    cli.Bind("pump_get_min_flowrate",[this,&cli](std::vector<std::string> args)->void{
+        uint8_t pump_count = pump_controller->Pump_count();
+        
+        if(not CLI_service::Check_argument_count(args, cli, 0, pump_count)){
+            return;
+        }
+        
+        std::vector<uint8_t> selected_indexes;
+        
+        if(not Parse_pump_indexes(args, pump_count, cli, true, selected_indexes)){
+            return;
+        }
+        
+        for(const auto& pump_index : selected_indexes){
+            std::optional<float> flowrate = pump_controller->Min_flowrate(pump_index);
+            
+            Print_pump_value(cli,pump_index,flowrate);
+        }
+    },"get minimal flowrate for all, or selected pumps","[pump_index pump_index ...]?");
+    
+    cli.Bind("pump_set_speed",[this,&cli](std::vector<std::string> args)->void{
+        uint8_t pump_count = pump_controller->Pump_count();
+        
+        if(not CLI_service::Check_argument_count(args, cli, 2, pump_count+1)){
+            return;
+        }
+        
+        float speed = 0.0;
+        if(not CLI_service::Parse_argument(args[0],cli,speed)){
+            return;
+        }
+        
+        std::vector<uint8_t> selected_indexes;
+        if(not Parse_pump_indexes(args, pump_count, cli, false, selected_indexes, 1)){
+            return;
+        }
+        
+        for(const auto& pump_index : selected_indexes){
+            if(not pump_controller->Set_speed(pump_index, speed)){
+                cli.Print_error("Set_speed failed");
+            }else{
+                cli.Print_ln("success");
+            }
+        }
+    },"set speed for selected pumps","speed(float) pump_index pump_index ...");
+    
+    cli.Bind("pump_set_flowrate",[this,&cli](std::vector<std::string> args)->void{
+        uint8_t pump_count = pump_controller->Pump_count();
+        
+        if(not CLI_service::Check_argument_count(args, cli, 2, pump_count+1)){
+            return;
+        }
+        
+        float flowrate = 0.0;
+        if(not CLI_service::Parse_argument(args[0],cli,flowrate)){
+            return;
+        }
+        
+        std::vector<uint8_t> selected_indexes;
+        if(not Parse_pump_indexes(args, pump_count, cli, false, selected_indexes, 1)){
+            return;
+        }
+        
+        for(const auto& pump_index : selected_indexes){
+            if(not pump_controller->Set_flowrate(pump_index, flowrate)){
+                cli.Print_error("Set_flowrate failed");
+            }else{
+                cli.Print_ln("success");
+            }
+        }
+    },"set flowrate for selected pumps","flowrate(float) pump_index pump_index ...");
+    
+    cli.Bind("pump_set_max_flowrate",[this,&cli](std::vector<std::string> args)->void{
+        uint8_t pump_count = pump_controller->Pump_count();
+        
+        if(not CLI_service::Check_argument_count(args, cli, 2, pump_count+1)){
+            return;
+        }
+        
+        float flowrate = 0.0;
+        if(not CLI_service::Parse_argument(args[0],cli,flowrate)){
+            return;
+        }
+        
+        std::vector<uint8_t> selected_indexes;
+        if(not Parse_pump_indexes(args, pump_count, cli, false, selected_indexes, 1)){
+            return;
+        }
+        
+        for(const auto& pump_index : selected_indexes){
+            if(not pump_controller->Set_max_flowrate(pump_index, flowrate)){
+                cli.Print_error("Set_max_flowrate failed");
+            }else{
+                cli.Print_ln("success");
+            }
+        }
+    },"set max flowrate for selected pumps", "flowrate(float) pump_index pump_index ...");
 }
