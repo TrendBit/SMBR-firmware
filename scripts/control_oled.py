9f7f7378212c4930a59bb8a6dc37d348cc3d2080
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-06-23 16:41:59 +0200

    Scripts: added control_oled.py for controlling oled display

---
+++ scripts/control_oled.py
@@ -0,0 +1,62 @@
+import time 
+
+import can
+import argparse
+
+from application_codes import Message, message_types, module_types, module_instances
+
+def print_to_oled(can_interface, message : str, verbose=False):
+    with can.interface.Bus(channel=can_interface, bustype='socketcan') as bus:
+        # split the message into chunks of 8 characters (to fit into the message)
+        message_chunks : list[str] = []
+        while message:
+            message_chunks.append(message[:8])
+            message = message[8:]
+        
+        for chunk in message_chunks:
+            can_message = Message(message_types["Mini_OLED_print_custom_text"], module_types["Any"], module_instances["All"],data=chunk.encode())
+    
+            try:
+                bus.send(can_message.can_message())
+                
+                # wait for 50ms so the buffer can catch up
+                time.sleep(0.05)
+                if verbose:
+                    print(f"Sending oled print chunk: '{chunk}'")
+            except can.CanError as e:
+                print(f"Failed to send message: {e}")
+                exit(1)
+
+def clear_oled(can_interface, verbose = False):
+    with can.interface.Bus(channel=can_interface, bustype='socketcan') as bus:
+        can_message = Message(message_types["Mini_OLED_clear_custom_text"], module_types["Any"], module_instances["All"])
+
+        try:
+            bus.send(can_message.can_message())
+            if verbose:
+                print(f"Sending clear oled message")
+        except can.CanError as e:
+            print(f"Failed to send message: {e}")
+            exit(1)
+    
+def replace_oled_text(can_interface, message : str, verbose = False):
+    clear_oled(can_interface, verbose)
+    time.sleep(0.1)
+    print_to_oled(can_interface, message, verbose)
+    
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description="Append text to oled or clear the display")
+    parser.add_argument('--interface', type=str, default='can0', help='CAN interface to use, default can0')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-t', '--text', type=str, help='What to print')
+    group.add_argument('-c', '--clear', action='store_true', help='Clear display')
+    group.add_argument('-r', '--replace', type=str, help='Clear display')
+    
+    args = parser.parse_args()
+    if args.replace:
+        replace_oled_text(args.interface, args.replace, verbose=True)
+    if args.text:
+        print_to_oled(args.interface, args.text, verbose=True)
+    if args.clear:
+        clear_oled(args.interface, verbose=True)
